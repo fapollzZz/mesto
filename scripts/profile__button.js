f0b21b2aa2167b82da6e9bd3deb77f436464eded
fapollzZz <kmilvat@mail.ru>
2022-09-30 00:09:11 +0700

    popup completed

--- scripts/profile__button.js
+++ scripts/profile__button.js
@@ -2,24 +2,36 @@ const popupOpenButton = document.querySelector('.profile__open-popup');
 const popup = document.querySelector('.popup');
 const popupCloseButton = popup.querySelector('.popup__close');
 
-let nameInput = popup.querySelector(".form__item_name");
-let descriptionInput = popup.querySelector(".form__item_composition");
+let profileName = document.getElementById("profileName");
+let profileAboutMe = document.getElementById("profileAbout");
 let userName = document.getElementById("name");
 let userDescription = document.getElementById("composition");
 let form = document.querySelector(".popup__container")
 
 
-function popupToggle() {
+function popupOpen() {
     popup.classList.toggle('popup__opened');
 }
-popupOpenButton.addEventListener('click', popupToggle);
-popupCloseButton.addEventListener('click', popupToggle);
 
-function popupSave(evt){
-    evt.preventDefault();
-    userName.textContent = nameInput.value;
-    userDescription.textContent = descriptionInput.value;
-    console.log(userName);
+function popupClose() {
+    popup.classList.toggle('popup__opened');
+    userName.value = "Жак-Ив Кусто";
+    profileName.value = "Жак-Ив Кусто";
+    userDescription.value = "Исследователь океана";
+    profileAboutMe.value = "Исследователь океана";
 }
+
+popupOpenButton.addEventListener('click', popupOpen);
+popupCloseButton.addEventListener('click', popupClose);
+
+function popupSave(evt) {
+    evt.preventDefault();
+    userName.textContent = userName.value;
+    userDescription.textContent = userDescription.value;
+    profileName.value = userName.textContent;
+    profileAboutMe.value = userDescription.textContent;
+    popupOpen();
+}
+
 
 form.addEventListener('submit', popupSave);
